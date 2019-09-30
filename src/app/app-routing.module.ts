8cfe3f76df726709aeaf8f0fcdd9e12c29427544
Manoj P <thedarkstrix@gmail.com>
2019-09-30 11:36:45 +0530

    Misc Changes
    ** Add New Home Module
    ** Main module is only used for routing , this will be needed further to create nav routing
    ** Add routes for the homeModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { MainComponent } from "./main/main.component";
 import { LocalmultiplayerComponent } from "./localmultiplayer/localmultiplayer.component";
 import { EasyComponent } from "./easy/easy.component";
 import { MinmaxComponent } from "./minmax/minmax.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: MainComponent
+    component: HomeComponent
   },
   {
     path: "LocalMultiplayer",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { MainComponent } from "./main/main.component";
 import { MinmaxComponent } from "./minmax/minmax.component";
 import { LocalmultiplayerComponent } from "./localmultiplayer/localmultiplayer.component";
 import { EasyComponent } from "./easy/easy.component";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     MinmaxComponent,
     LocalmultiplayerComponent,
     EasyComponent,
-    MainComponent
+    MainComponent,
+    HomeComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
