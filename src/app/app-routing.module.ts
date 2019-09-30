eec3d6041648e9794bc62299b1958d929a6ed558
Manoj P <thedarkstrix@gmail.com>
2019-09-30 11:24:54 +0530

    Add Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainComponent } from "./main/main.component";
+import { LocalmultiplayerComponent } from "./localmultiplayer/localmultiplayer.component";
+import { EasyComponent } from "./easy/easy.component";
+import { MinmaxComponent } from "./minmax/minmax.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent
+  },
+  {
+    path: "LocalMultiplayer",
+    component: LocalmultiplayerComponent
+  },
+  {
+    path: "easywithAI",
+    component: EasyComponent
+  },
+  {
+    path: "hardwithAI",
+    component: MinmaxComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
